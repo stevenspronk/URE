ba71b362f5d75b320af32545d9d53fcc12d3d70a
stevenspronk <stevenspronk@gmail.com>
2016-03-08 08:52:32 +0000

    changes

--- Component.js
+++ Component.js
@@ -1,65 +1,19 @@
 jQuery.sap.declare("com.URE.Component");
 sap.ui.core.UIComponent.extend("com.URE.Component", {
-	metadata: {
-		routing: {
-			config: {
-				viewType: "XML",
-				viewPath: "MVC",
-				targetControl: "navContainer",
-				targetAggregation: "pages",
-				clearTarget: false
-			},
-			routes: [{
-				pattern: "",
-				name: "CreateTest",
-				view: "CreateTest"
-			}, {
-				pattern: "Overview/{id}",
-				name: "Overview",
-				view: "Overview"
-			}]
-		},
-		config: {
-			"serviceConfig": {
-				"name": "UreRaceData.OData.UreOdataService",
-				"serviceUrl": "/destinations/McCoy_URE/UreOdataService.xsodata/"
-			},
-			
-			"messageService":{
-				"name": "UreRaceData.OData.Messages",
-				"serviceUrl": "/destinations/McCoy_URE/Messages.xsodata/"}
-		}
-	}
+	  metadata : {
+            manifest: "json"
+      }
 });
 com.URE.Component.prototype.init = function() {
 	jQuery.sap.require("sap.ui.core.routing.History");
 	jQuery.sap.require("sap.m.routing.RouteMatchedHandler");
 	sap.ui.core.UIComponent.prototype.init.apply(this);
-	var router = this.getRouter();
-	this.routeHandler = new sap.m.routing.RouteMatchedHandler(router);
-	router.initialize();
 
-
-	     // Create a resource bundle for language specific texts
-      var oResourceModel = new sap.ui.model.resource.ResourceModel({
-        bundleName : "com.URE.model.i18n"
-      });
-
-      // Assign the model object to the SAPUI5 core using the name Messages"
-      sap.ui.getCore().setModel(oResourceModel, "i18n");
-
-
-
-	//RaceMetaData model
-		var oRaceMetaData = new sap.ui.model.json.JSONModel();
-    	oRaceMetaData.loadData("./model/RaceMetaData.json");
-    	sap.ui.getCore().setModel(oRaceMetaData, "RaceMetaData");
-      
-
-
+	this.getRouter().initialize();
 
 
 };
+
 com.URE.Component.prototype.destroy = function() {
 	if (this.routeHandler) {
 		this.routeHandler.destroy();
--- MVC/CreateTest.controller.js
+++ MVC/CreateTest.controller.js
@@ -1,51 +1,72 @@
 sap.ui.controller("MVC.CreateTest", {
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf MVC.CreateTest
-*/
-//	onInit: function() {
-//
-//	},
+	/**
+	 * Called when a controller is instantiated and its View controls (if available) are already created.
+	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+	 * @memberOf MVC.CreateTest
+	 */
+	onInit: function() {
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf MVC.CreateTest
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+		//RaceMetaData model
+		var oRaceMetaData = new sap.ui.model.odata.v2.ODataModel("/destinations/McCoy_URE/UreMetadata.xsodata/");
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf MVC.CreateTest
-*/
-//	onAfterRendering: function() {
-//
-//	},
+		oRaceMetaData.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
 
-/**
-* Called when thse Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf MVC.CreateTest
-*/
-//	onExit: function() {
-//
-//	}
+		oRaceMetaData.attachMetadataLoaded(null, function() {
+			var oMetadata = oRaceMetaData.getServiceMetadata();
+			console.log(oMetadata);
+			var raceContext = oRaceMetaData.createEntry("URE_METADATA", {
+				RACE_ID: "Test_race1",
+				START_TIME: new Date().getTime()
+			});
 
-	startTest  :function(){
-		
-	/** form validatie hier */
+			this.getView().setModel(raceContext, "RaceMetaData");
+		}, null);
+
+	},
+
+	/**
+	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+	 * (NOT before the first rendering! onInit() is used for that one!).
+	 * @memberOf MVC.CreateTest
+	 */
+	//	onBeforeRendering: function() {
+	//
+	//	},
+
+	/**
+	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+	 * This hook is the same one that SAPUI5 controls get after being rendered.
+	 * @memberOf MVC.CreateTest
+	 */
+	//	onAfterRendering: function() {
+	//
+	//	},
+
+	/**
+	 * Called when thse Controller is destroyed. Use this one to free resources and finalize activities.
+	 * @memberOf MVC.CreateTest
+	 */
+	//	onExit: function() {
+	//
+	//	}
+
+	startTest: function() {
+		//First store entry in metadata table
+		var timestamp = new Date().getTime();
+		var oRaceMetaData = this.getView().getModel("RaceMetaData");
+
+		oRaceMetaData.submitChanges();
+		//Then, navigate to overview
 		this.goToOverview();
 	},
-	
-	goToOverview  :function(){
-		
+
+	goToOverview: function() {
+
 		var router = sap.ui.core.UIComponent.getRouterFor(this);
-		router.navTo("Overview", {id:1}, false);
+		router.navTo("Overview", {
+			id: 1
+		}, false);
 	}
-	
-	
+
 });
