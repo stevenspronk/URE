340e25d8b4feca2180dfb9b02ee93ad01fde0495
Fab1Man <fabiancamps@gmail.com>
2016-04-13 13:12:30 +0000

    Stop Button

--- MVC/History.controller.js
+++ MVC/History.controller.js
@@ -1,92 +1,95 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
 	"use strict";
 	return Controller.extend("MVC.History", {
 		/**
 		 *@memberOf ProfileServices.controller.Overview
 		 */
-selectHistory: function () {
+		selectHistory: function() {
 			var oTable = this.byId("__table0");
 			var oSelectedItem = oTable.getSelectedItem();
 			var sPath = oSelectedItem.getBindingContext().getPath();
 			var oDetail = this.getView().getModel().getProperty(sPath);
 			raceID = oDetail.RACE_ID;
 			runID = oDetail.RUN_ID;
-			
+
 			var oModel = new sap.ui.model.json.JSONModel({
 				raceID: raceID,
 				runID: runID
 			});
-			 // var end_date = new Date(oDetail.DateEnd);
-             // var start_time = new Date(oDetail.TimeStart.ms).getUTCHours() + ":" + new Date(oDetail.TimeStart.ms).getUTCMinutes() + ":" + new Date(oDetail.TimeStart.ms).getUTCSeconds();
-             // var end_time = new Date(oDetail.TimeEnd.ms).getUTCHours() + ":" + new Date(oDetail.TimeEnd.ms).getUTCMinutes() + ":" + new Date(oDetail.TimeStart.ms).getUTCSeconds();
-             // this.getView().byId("txtStartDate").setDateValue(start_date);
-             // this.getView().byId("txtStartTime").setValue(start_time);
-             // this.getView().byId("txtEndDate").setDateValue(end_date);
-             // this.getView().byId("txtEndTime").setValue(end_time);
-             // this.getView().byId("intensityRating").setValue(parseFloat(oDetail.Intensity));
-             // this.getView().byId("location").setSelectedKey(oDetail.LocationId);
-             // this.getView().byId("userId").setText(oDetail.UserId);
-             // this.getView().byId("comment").setValue(oDetail.Comment);
+			sap.ui.getCore().setModel(oModel, "ID");
+			
+			debugger;
+			// var end_date = new Date(oDetail.DateEnd);
+			// var start_time = new Date(oDetail.TimeStart.ms).getUTCHours() + ":" + new Date(oDetail.TimeStart.ms).getUTCMinutes() + ":" + new Date(oDetail.TimeStart.ms).getUTCSeconds();
+			// var end_time = new Date(oDetail.TimeEnd.ms).getUTCHours() + ":" + new Date(oDetail.TimeEnd.ms).getUTCMinutes() + ":" + new Date(oDetail.TimeStart.ms).getUTCSeconds();
+			// this.getView().byId("txtStartDate").setDateValue(start_date);
+			// this.getView().byId("txtStartTime").setValue(start_time);
+			// this.getView().byId("txtEndDate").setDateValue(end_date);
+			// this.getView().byId("txtEndTime").setValue(end_time);
+			// this.getView().byId("intensityRating").setValue(parseFloat(oDetail.Intensity));
+			// this.getView().byId("location").setSelectedKey(oDetail.LocationId);
+			// this.getView().byId("userId").setText(oDetail.UserId);
+			// this.getView().byId("comment").setValue(oDetail.Comment);
 		},
-		onNavBack: function () {
+		onNavBack: function() {
 			window.history.go(-1);
 		},
-		updateEntry: function () {
-			var oEntry = {};
-			oEntry.UserId = this.getView().byId("userId").getText();
-			oEntry.DateStart = this.getView().byId("txtStartDate").getValue();
-			oEntry.TimeStart = this.getView().byId("txtStartTime").getValue();
-			oEntry.DateEnd = this.getView().byId("txtEndDate").getValue();
-			oEntry.TimeEnd = this.getView().byId("txtEndTime").getValue();
-			oEntry.Intensity = this.getView().byId("intensityRating").getValue().toString();
-			if (this.getView().byId("vomit").getSelected() === true) {
-				oEntry.Vomit = "X";
-			} else {
-				oEntry.Vomit = " ";
-			}
-			if (this.getView().byId("nausea").getSelected() === true) {
-				oEntry.Nausea = "X";
-			} else {
-				oEntry.Nausea = " ";
-			}
-			if (this.getView().byId("hypersensitivity").getSelected() === true) {
-				oEntry.Hypersensitivity = "X";
-			} else {
-				oEntry.Hypersensitivity = " ";
-			}
-			oEntry.LocationId = parseInt(this.getView().byId("location").getSelectedKey());
-			oEntry.Comment = this.getView().byId("comment").getValue();
-			var splitTime = oEntry.TimeStart.split(":");
-			var timekey = "PT" + splitTime[0] + "H" + splitTime[1] + "M" + splitTime[2] + "S";
-			oEntry.TimeStart = timekey;
-			var splitTimeEnd = oEntry.TimeEnd.split(":");
-			var timekeyend = "PT" + splitTimeEnd[0] + "H" + splitTimeEnd[1] + "M" + splitTimeEnd[2] + "S";
-			oEntry.TimeEnd = timekeyend;
-			var datesplit = oEntry.DateStart.split("/");
-			var tempDateStart = new Date(datesplit[2], datesplit[1], datesplit[0]);
-			//	console.log(tempDateStart);
-var datekey = tempDateStart.getFullYear() + "-" + (tempDateStart.getMonth() + 1) + "-" + tempDateStart.getDate() + "T00:00:00.0000000";
-			oEntry.DateStart = datekey;
-			var datesplit2 = oEntry.DateEnd.split("/");
-			var tempDateEnd = new Date(datesplit2[2], datesplit2[1], datesplit2[0]);
-			var datekeyend = "/Date(" + tempDateEnd.getTime() + ")/";
-			oEntry.DateEnd = datekeyend;
-			var oModel = this.getView().getModel();
-			var sPath = "/Attack(UserId='" + oEntry.UserId + "',DateStart=datetime'" + datekey + "',TimeStart=time'" + timekey + "')";
-			//var sPath = "Attack('" + oEntry.UserId + "','" + datekey + "','"+timekey+"')";
-console.log(oEntry);
-			oModel.update(sPath, oEntry, null, function () {
-				console.log("update succeeded");
-			}, function () {
-				console.log("update failed");
-			});
-		},
+		// 		updateEntry: function () {
+		// 			var oEntry = {};
+		// 			oEntry.UserId = this.getView().byId("userId").getText();
+		// 			oEntry.DateStart = this.getView().byId("txtStartDate").getValue();
+		// 			oEntry.TimeStart = this.getView().byId("txtStartTime").getValue();
+		// 			oEntry.DateEnd = this.getView().byId("txtEndDate").getValue();
+		// 			oEntry.TimeEnd = this.getView().byId("txtEndTime").getValue();
+		// 			oEntry.Intensity = this.getView().byId("intensityRating").getValue().toString();
+		// 			if (this.getView().byId("vomit").getSelected() === true) {
+		// 				oEntry.Vomit = "X";
+		// 			} else {
+		// 				oEntry.Vomit = " ";
+		// 			}
+		// 			if (this.getView().byId("nausea").getSelected() === true) {
+		// 				oEntry.Nausea = "X";
+		// 			} else {
+		// 				oEntry.Nausea = " ";
+		// 			}
+		// 			if (this.getView().byId("hypersensitivity").getSelected() === true) {
+		// 				oEntry.Hypersensitivity = "X";
+		// 			} else {
+		// 				oEntry.Hypersensitivity = " ";
+		// 			}
+		// 			oEntry.LocationId = parseInt(this.getView().byId("location").getSelectedKey());
+		// 			oEntry.Comment = this.getView().byId("comment").getValue();
+		// 			var splitTime = oEntry.TimeStart.split(":");
+		// 			var timekey = "PT" + splitTime[0] + "H" + splitTime[1] + "M" + splitTime[2] + "S";
+		// 			oEntry.TimeStart = timekey;
+		// 			var splitTimeEnd = oEntry.TimeEnd.split(":");
+		// 			var timekeyend = "PT" + splitTimeEnd[0] + "H" + splitTimeEnd[1] + "M" + splitTimeEnd[2] + "S";
+		// 			oEntry.TimeEnd = timekeyend;
+		// 			var datesplit = oEntry.DateStart.split("/");
+		// 			var tempDateStart = new Date(datesplit[2], datesplit[1], datesplit[0]);
+		// 			//	console.log(tempDateStart);
+		// var datekey = tempDateStart.getFullYear() + "-" + (tempDateStart.getMonth() + 1) + "-" + tempDateStart.getDate() + "T00:00:00.0000000";
+		// 			oEntry.DateStart = datekey;
+		// 			var datesplit2 = oEntry.DateEnd.split("/");
+		// 			var tempDateEnd = new Date(datesplit2[2], datesplit2[1], datesplit2[0]);
+		// 			var datekeyend = "/Date(" + tempDateEnd.getTime() + ")/";
+		// 			oEntry.DateEnd = datekeyend;
+		// 			var oModel = this.getView().getModel();
+		// 			var sPath = "/Attack(UserId='" + oEntry.UserId + "',DateStart=datetime'" + datekey + "',TimeStart=time'" + timekey + "')";
+		// 			//var sPath = "Attack('" + oEntry.UserId + "','" + datekey + "','"+timekey+"')";
+		// console.log(oEntry);
+		// 			oModel.update(sPath, oEntry, null, function () {
+		// 				console.log("update succeeded");
+		// 			}, function () {
+		// 				console.log("update failed");
+		// 			});
+		// 		},
 		/**
 		 *@memberOf MVC.History
 		 */
-onInit: function () {
+		onInit: function() {
 			//This code was generated by the layout editor.
-var oRaceHistory = new sap.ui.model.odata.ODataModel("/destinations/McCoy_URE/UreMetadata.xsodata/");
+			var oRaceHistory = new sap.ui.model.odata.ODataModel("/destinations/McCoy_URE/UreMetadata.xsodata/");
 			oRaceHistory.oHeaders = {
 				"DataServiceVersion": "3.0",
 				"MaxDataServiceVersion": "3.0"
@@ -94,11 +97,11 @@ var oRaceHistory = new sap.ui.model.odata.ODataModel("/destinations/McCoy_URE/Ur
 			this.getView().setModel(oRaceHistory);
 		},
 		/**
-	*@memberOf MVC.History
-	*/
-goToHistoryDashboard: function () {
-var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("Overview", {        //Router navigation is done in manifest.json Code Editor
+		 *@memberOf MVC.History
+		 */
+		goToHistoryDashboard: function() {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("Overview", { //Router navigation is done in manifest.json Code Editor
 				id: 1
 			}, false);
 		}
--- MVC/Overview.controller.js
+++ MVC/Overview.controller.js
@@ -2,15 +2,15 @@ var activeView;
 
 sap.ui.controller("MVC.Overview", {
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf MVC.Overview
-*/
+	/**
+	 * Called when a controller is instantiated and its View controls (if available) are already created.
+	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+	 * @memberOf MVC.Overview
+	 */
 	onInit: function() {
 		this.router = sap.ui.core.UIComponent.getRouterFor(this);
 		this.router.attachRoutePatternMatched(this._handleRouteMatched, this);
-		
+
 		//Create a model to store in which tab we are (filled from the Overview.controller.js)
 		//We fill it initially with the Dashboard view
 		var oSelection = new sap.ui.model.json.JSONModel({
@@ -19,36 +19,38 @@ sap.ui.controller("MVC.Overview", {
 		sap.ui.getCore().setModel(oSelection, "Selection");
 
 	},
-	
+
 	onSelect: function(oEvent) {
-            var key = oEvent.getParameters().key;
-			var oSelection = sap.ui.getCore().getModel("Selection");
-			oSelection.oData.selectedView = key;
-    },
-    
-	_handleRouteMatched:function(evt){
-		if("Overview" !== evt.getParameter("name")){
+		var key = oEvent.getParameters().key;
+		var oSelection = sap.ui.getCore().getModel("Selection");
+		oSelection.oData.selectedView = key;
+	},
+
+	_handleRouteMatched: function(evt) {
+		if ("Overview" !== evt.getParameter("name")) {
 			return;
 		}
 		var id = evt.getParameter("arguments").id;
-		var model = new sap.ui.model.json.JSONModel({id:id});
-		this.getView().setModel(model,"data");
+		var model = new sap.ui.model.json.JSONModel({
+			id: id
+		});
+		this.getView().setModel(model, "data");
 	},
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf MVC.Overview
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+	/**
+	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+	 * (NOT before the first rendering! onInit() is used for that one!).
+	 * @memberOf MVC.Overview
+	 */
+	//	onBeforeRendering: function() {
+	//
+	//	},
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf MVC.Overview
-*/
+	/**
+	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+	 * This hook is the same one that SAPUI5 controls get after being rendered.
+	 * @memberOf MVC.Overview
+	 */
 	onAfterRendering: function() {
 		// The app should refresh the data continuesly, but not all models, only the models of the active tab
 		var me = this;
@@ -90,27 +92,72 @@ sap.ui.controller("MVC.Overview", {
 		}
 	},
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf MVC.Overview
-*/
-//	onExit: function() {
-//
-//	}
-	onNavBack:function(){
-		
+	/**
+	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+	 * @memberOf MVC.Overview
+	 */
+	//	onExit: function() {
+	//
+	//	}
+	onNavBack: function() {
+
 		window.history.go(-1);
 	},
-	
-	navButtonTap:function(){
-              window.history.go(-1);
-              },
-              
-	newTest  :function(){
-		var router = sap.ui.core.UIComponent.getRouterFor(this);
+
+	navButtonTap: function() {
+		window.history.go(-1);
+	},
+
+	newTest: function() {
+		var oRaceMetaData = new sap.ui.model.odata.ODataModel("/destinations/McCoy_URE/UreMetadata.xsodata/");
+		//var oRaceModel = sap.ui.getCore().byId;
+
+		var sPath = "/URE_METADATA(RACE_ID=" + raceID + ",RUN_ID=" + runID + ")";
+
+		//oRaceMetaData.getData(sPath);
+
+		oRaceMetaData.read(sPath, function() {
+			console.log("update succeeded");
+		}, function() {
+			console.log("update failed");
+		});
+
+		debugger;
 		
-		// Set variable crudTest to C = Create
+		// Set END_TIME
+		var data = {
+			"RACE_ID": raceID,
+			"RUN_ID": runID,
+			"CIRCUIT": oRaceMetaData.CIRCUIT,
+			"TEMPERATURE": oRaceMetaData.TEMPERATURE,
+			"RACE_DESCRIPTION": oRaceMetaData.RACE_DESCRIPTION,
+			"START_TIME": oRaceMetaData.START_TIME,
+			"END_TIME": new Date(),
+			"RACE_TYPE": oRaceMetaData.RACE_TYPE,
+			"WEATHER": oRaceMetaData.WEATHER,
+			"NOTES": oRaceMetaData.NOTES,
+			"CAR_ID": oRaceMetaData.CAR_ID,
+			"CAR_NOTES": oRaceMetaData.CAR_NOTES,
+			"NAME_DRIVER": oRaceMetaData.NAME_DRIVER,
+			"LENGTH_DRIVER": oRaceMetaData.LENGTH_DRIVER,
+			"WEIGHT_DRIVER": oRaceMetaData.WEIGHT_DRIVER,
+			"DRIVER_NOTES": oRaceMetaData.DRIVER_NOTES
+		};
+
+		oRaceMetaData.update(sPath, data, function() {
+			console.log("update succeeded");
+		}, function() {
+			console.log("update failed");
+		});
+
+		var router = sap.ui.core.UIComponent.getRouterFor(this);
+		router.navTo("CreateTest", {
+			id: 1
+		}, false);
+
+		// Set variable crudTest to C = Create		
 		crudTest = 'C';
-		router.navTo("CreateTest", {id:1}, false);
+
+		sap.m.MessageToast.show("Test gestopt en opgeslagen");
 	}
 });
