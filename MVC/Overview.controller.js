85acd01a2984e08934da4432be289965dc8081b7
stevenspronk <stevenspronk@gmail.com>
2016-04-22 08:33:17 +0000

    ffd

--- MVC/Overview.controller.js
+++ MVC/Overview.controller.js
@@ -50,20 +50,8 @@ sap.ui.controller("MVC.Overview", {
 		wait = false;
 	},
 
-	/**
-	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-	 * (NOT before the first rendering! onInit() is used for that one!).
-	 * @memberOf MVC.Overview
-	 */
-	//	onBeforeRendering: function() {
-	//
-	//	},
 
-	/**
-	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-	 * This hook is the same one that SAPUI5 controls get after being rendered.
-	 * @memberOf MVC.Overview
-	 */
+
 	onAfterRendering: function() {
 		// The app should refresh the data continuesly, but not all models, only the models of the active tab
 		var me = this;
